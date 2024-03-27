5694305b5dd1425acd21ea77da813fc69d0ad9be
abhi23792 <abhi.23792@gmail.com>
2024-03-27 14:27:12 +0530

    Add redux toolkit for the project. (#2)
    Add redux toolkit for the project.

---
+++ src/slice/ProductSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Product } from "../models/Product";
+
+type ProductState = {
+    products: Array<Product>;
+}
+
+const initialState: ProductState = {
+    products: []
+};
+
+const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {
+        getProducts: (state, action) => {
+            state.products += action.payload;
+        }
+    }
+});
+
+export const productActions = productSlice.actions;
+
+export default productSlice.reducer;
+
